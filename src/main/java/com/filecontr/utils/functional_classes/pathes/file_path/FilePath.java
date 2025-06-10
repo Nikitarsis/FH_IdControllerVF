b4ceff8aef0096c175055a109d3a64362ec9da91
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-11 00:16:25 +0300

    Add annotations

--- src/main/java/com/filecontr/utils/functional_classes/pathes/file_path/FilePath.java
+++ src/main/java/com/filecontr/utils/functional_classes/pathes/file_path/FilePath.java
@@ -1,8 +1,10 @@
 package com.filecontr.utils.functional_classes.pathes.file_path;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class FilePath {
   String type;
   String relativePath;
--- src/main/java/com/filecontr/utils/functional_classes/pathes/server_path/ServerPath.java
+++ src/main/java/com/filecontr/utils/functional_classes/pathes/server_path/ServerPath.java
@@ -2,9 +2,11 @@ package com.filecontr.utils.functional_classes.pathes.server_path;
 
 import com.filecontr.utils.functional_classes.pathes.file_path.FilePath;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ServerPath {
   String serverURL;
   FilePath file;
