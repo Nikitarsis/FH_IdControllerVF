890b842bd5dce6a376a4c19630abe3898f89ee5b
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-12 15:36:20 +0300

    Add ServerPath

--- src/main/java/com/filecontr/utils/functional_classes/content/DefaultContent.java
+++ src/main/java/com/filecontr/utils/functional_classes/content/DefaultContent.java
@@ -2,9 +2,15 @@ package com.filecontr.utils.functional_classes.content;
 
 import java.util.HashMap;
 
+import com.filecontr.utils.functional_classes.pathes.server_path.ServerPath;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class DefaultContent implements IContent {
   HashMap<Long, String> relations;
   Long creationTime;
+  ServerPath path;
 
   @Override
   public HashMap<Long, String> getRelations() {
@@ -14,5 +20,10 @@ public class DefaultContent implements IContent {
   @Override
   public Long getCreationTime() {
     return creationTime;
+  }
+
+  @Override
+  public ServerPath getServerPath() {
+    return path;
   }
 }
--- src/main/java/com/filecontr/utils/functional_classes/content/IContent.java
+++ src/main/java/com/filecontr/utils/functional_classes/content/IContent.java
@@ -2,7 +2,10 @@ package com.filecontr.utils.functional_classes.content;
 
 import java.util.HashMap;
 
+import com.filecontr.utils.functional_classes.pathes.server_path.ServerPath;
+
 public interface IContent {
   HashMap<Long, String> getRelations();
   Long getCreationTime();
+  ServerPath getServerPath();
 }
