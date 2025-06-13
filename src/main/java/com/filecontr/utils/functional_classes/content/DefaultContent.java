eb22e0d5d87469a902b748d7abb8df5cfef5287c
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-13 14:42:30 +0300

    Removed Relations

--- src/main/java/com/filecontr/utils/functional_classes/content/DefaultContent.java
+++ src/main/java/com/filecontr/utils/functional_classes/content/DefaultContent.java
@@ -1,21 +1,13 @@
 package com.filecontr.utils.functional_classes.content;
 
-import java.util.HashMap;
-
 import com.filecontr.utils.functional_classes.pathes.server_path.ServerPath;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 class DefaultContent implements IContent {
-  HashMap<Long, String> relations;
   Long creationTime;
   ServerPath path;
-
-  @Override
-  public HashMap<Long, String> getRelations() {
-    return relations;
-  }
 
   @Override
   public Long getCreationTime() {
--- src/main/java/com/filecontr/utils/functional_classes/content/IContent.java
+++ src/main/java/com/filecontr/utils/functional_classes/content/IContent.java
@@ -1,11 +1,8 @@
 package com.filecontr.utils.functional_classes.content;
 
-import java.util.HashMap;
-
 import com.filecontr.utils.functional_classes.pathes.server_path.ServerPath;
 
 public interface IContent {
-  HashMap<Long, String> getRelations();
   Long getCreationTime();
   ServerPath getServerPath();
 }
