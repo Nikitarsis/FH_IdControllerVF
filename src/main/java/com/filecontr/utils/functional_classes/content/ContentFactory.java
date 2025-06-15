459d1bd4193a4b249ec0180892581f9f036c11e5
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-15 12:04:14 +0300

    Modified content factory

--- src/main/java/com/filecontr/utils/functional_classes/content/ContentFactory.java
+++ src/main/java/com/filecontr/utils/functional_classes/content/ContentFactory.java
@@ -1,15 +1,13 @@
 package com.filecontr.utils.functional_classes.content;
 
-import java.util.HashMap;
-
 import com.filecontr.utils.functional_classes.pathes.server_path.ServerPath;
 
 public class ContentFactory {
-  public static IContent createContent(HashMap<Long, String> relations, Long creationTime, ServerPath path) {
-    return new DefaultContent(relations, creationTime, path);
+  public static IContent createContent(Long creationTime, ServerPath path) {
+    return new DefaultContent(creationTime, path);
   }
 
   public static IContent createEmptyContent(ServerPath path) {
-    return createContent(new HashMap<Long, String>(), System.currentTimeMillis(), path);
+    return createContent(System.currentTimeMillis(), path);
   }
 }
