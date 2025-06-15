0929b3949155acb20a7245b28417bde3bf316487
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-15 12:06:03 +0300

    Mod stub server data

--- src/main/java/com/filecontr/utils/functional_classes/server_data/StubServerData.java
+++ src/main/java/com/filecontr/utils/functional_classes/server_data/StubServerData.java
@@ -1,5 +1,11 @@
 package com.filecontr.utils.functional_classes.server_data;
 
 public class StubServerData implements IServerData {
+
+  @Override
+  public String getURL() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getURL'");
+  }
   
 }
