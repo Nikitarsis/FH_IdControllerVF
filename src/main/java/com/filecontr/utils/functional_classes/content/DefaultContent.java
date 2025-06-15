c9ad00c454739eb7c80e215ff6d4553ddffbbe96
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-15 12:43:23 +0300

    Modified default content

--- src/main/java/com/filecontr/utils/functional_classes/content/DefaultContent.java
+++ src/main/java/com/filecontr/utils/functional_classes/content/DefaultContent.java
@@ -3,19 +3,11 @@ package com.filecontr.utils.functional_classes.content;
 import com.filecontr.utils.functional_classes.pathes.server_path.ServerPath;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
 class DefaultContent implements IContent {
   Long creationTime;
-  ServerPath path;
-
-  @Override
-  public Long getCreationTime() {
-    return creationTime;
-  }
-
-  @Override
-  public ServerPath getServerPath() {
-    return path;
-  }
+  ServerPath serverPath;
 }
