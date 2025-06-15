cd8d21bcb4d4ee459e33388a69d2593cedafa527
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-15 11:45:39 +0300

    Changed logger factory

--- src/main/java/com/filecontr/utils/adapters/logger/AdapterLoggerFactory.java
+++ src/main/java/com/filecontr/utils/adapters/logger/AdapterLoggerFactory.java
@@ -4,4 +4,8 @@ public class AdapterLoggerFactory {
   public static ILogger getLogger(Class<?> logClass) {
     return new SimpleLogger(logClass);
   }
+
+  public static ILogger getTestLogger(Class<?> logClass) {
+    return new SimpleLogger(logClass);
+  }
 }
