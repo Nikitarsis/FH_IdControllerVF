9e65b535a7e816d0ae45fb842f08247f08467713
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-24 01:44:57 +0300

    Minor hello controller

--- src/main/java/com/filecontr/controller/HelloController.java
+++ src/main/java/com/filecontr/controller/HelloController.java
@@ -1,17 +1,13 @@
 package com.filecontr.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @Autowired
-    NamedParameterJdbcTemplate jdbcTemplate;
-
-    @RequestMapping("/")
-    String hello() {
-        return "Hello!!! World!";
+    @GetMapping("/say-hello/{name}")
+    String hello(@PathVariable String name) {
+        return String.format("Hello, %s!", name);
     }
 }
