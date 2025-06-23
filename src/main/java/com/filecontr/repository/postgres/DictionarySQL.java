2f0bf5bc49c9582634eeb656849779e3e10caca7
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-24 01:33:57 +0300

    Modified requests

--- src/main/java/com/filecontr/repository/postgres/DictionarySQL.java
+++ src/main/java/com/filecontr/repository/postgres/DictionarySQL.java
@@ -1,24 +1,42 @@
 package com.filecontr.repository.postgres;
 
-import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public record DictionarySQL(
-  String GET_VIRTUAL_FILE
+  String GET_VIRTUAL_FILE,
+  String GET_RELATIONS,
+  String ADD_VIRTUAL_FILE,
+  String UPDATE_VIRTUAL_FILE,
+  String DELETE_VIRTUAL_FILE
 ) {
-  public static DictionarySQL parseFromResource() {
-    String text;
-    String path = "/databases/requests";
-    try(var scanner = new Scanner(DictionarySQL.class.getResourceAsStream(path)).useDelimiter(Pattern.compile("\A"))) {
-      text = scanner.hasNext()? scanner.next() : "";
-    }
-    var map = Arrays.stream(text.split("\n"))
-      .map(a -> a.split("--"))
-      .collect(Collectors.toMap(a -> a[0], a -> a[1]));
+
+  protected static DictionarySQL createFromMap(Function<String, String> dictionaryMapper) {
     return new DictionarySQL(
-      map.get("GET_VIRTUAL_FILE")
+      dictionaryMapper.apply("GET_VIRTUAL_FILE"),
+      dictionaryMapper.apply("GET_RELATIONS"),
+      dictionaryMapper.apply("ADD_VIRTUAL_FILE"),
+      dictionaryMapper.apply("UPDATE_VIRTUAL_FILE"),
+      dictionaryMapper.apply("DELETE_VIRTUAL_FILE")
     );
+  }
+
+  public static Optional<DictionarySQL> parseFromResource() {
+    final String path = "/databases/requests";
+    try(var scanner = new Scanner(DictionarySQL.class.getResourceAsStream(path)).useDelimiter(Pattern.compile("\n"))) {
+      final Map<String, String> map = scanner.tokens().map(a -> a.split("--"))
+      .collect(Collectors.toMap(a -> a[0], a -> a[1]));
+      Function<String, String> func = (String key) -> {
+        if (map.containsKey(key)) {
+          return map.get(key);
+        }
+        throw new RuntimeException(String.format("No such SQL Request name: %s", key));
+      };
+      return Optional.of(createFromMap(func));
+    }
   }
 }
--- src/main/java/com/filecontr/repository/postgres/VirtualFileRepository.java
+++ src/main/java/com/filecontr/repository/postgres/VirtualFileRepository.java
@@ -1,23 +1,62 @@
 package com.filecontr.repository.postgres;
 
 import java.sql.ResultSet;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.filecontr.service.virtual_files.IVirtualFile;
+
 @Repository
 public class VirtualFileRepository {
   private final NamedParameterJdbcTemplate jdbcTemplate;
+  private final DictionarySQL dictionary;
 
-  VirtualFileRepository(NamedParameterJdbcTemplate template) {
+  public VirtualFileRepository(NamedParameterJdbcTemplate template, DictionarySQL dictionarySQL) {
     this.jdbcTemplate = template;
+    this.dictionary = dictionarySQL;
   }
 
-  public Optional<String> getById(int id, Function<ResultSet, Optional<String>> exec) {
-    var params = new MapSqlParameterSource("id", id);
-    return jdbcTemplate.query("null", params, exec::apply);
+  public Optional<IVirtualFile> getVirtualFileById(Function<ResultSet, Optional<IVirtualFile>> exec, Long... ids) {
+    var value = Arrays.stream(ids).map(id -> id.toString()).collect(Collectors.joining(","));
+    var params = new MapSqlParameterSource("id", value);
+    return jdbcTemplate.query(dictionary.GET_VIRTUAL_FILE(), params, exec::apply);
+  }
+
+  public Optional<Map<Long, Long>> getRelations(Function<ResultSet, Optional<Map<Long, Long>>> exec) {
+    return jdbcTemplate.query(dictionary.GET_RELATIONS(), exec::apply);
+  }
+
+  public Boolean addVirtualFile(Function<ResultSet, Optional<IVirtualFile>> exec, IVirtualFile... files) {
+    var stream = Arrays.stream(files);
+    String valuesRelations = stream
+      .map(a -> String.format(
+        "(%d, %d)",
+        a.getId().toLong(),
+        a.getParentId().get().toLong()
+        )
+      )
+      .collect(Collectors.joining(", "));
+    String valuesProperties = stream
+      .map(a -> String.format(
+        "(%d, %s, %d, %d)",
+        a.getId().toLong(),
+        a.getContent().getFilePath().directoryPath(),
+        a.getContent().getFilePath().parentId().get().toLong(),
+        a.getContent().getCreationTime()
+        )
+      )
+      .collect(Collectors.joining(", "));
+    var params = new MapSqlParameterSource();
+    params.addValue("valuesProperties", valuesProperties);
+    params.addValue("valuesRelations", valuesRelations);  
+    jdbcTemplate.query(dictionary.ADD_VIRTUAL_FILE(), params, exec::apply);
+    return true;
   }
 }
