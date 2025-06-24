3b77d07d3273e9d7ea2eae8010489e2ea0a5ef07
Nikita <Nik-Mir-Vas@yandex.ru>
2025-06-24 15:04:59 +0300

    Modified tests

--- src/test/java/com/filecontr/utils/functional_classes/content/TestContent.java
+++ src/test/java/com/filecontr/utils/functional_classes/content/TestContent.java
@@ -3,21 +3,21 @@ package com.filecontr.utils.functional_classes.content;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import com.filecontr.utils.functional_classes.pathes.file_path.FilePath;
+import com.filecontr.utils.functional_classes.pathes.file_data.FileData;
 
 public class TestContent {
   @Test
   void testCompleteFactoryMethod() {
-    var filePath = FilePath.createTestFilePath();
+    var filePath = FileData.createTestFileData();
     var time = 1488l;
     var content = ContentFactory.createContent(time, filePath);
     Assertions.assertEquals(time, content.getCreationTime());
-    Assertions.assertEquals(filePath, content.getFilePath());   
+    Assertions.assertEquals(filePath, content.getFileData());   
   }
 
   @Test
   void testSimpleMethod() {
-    var filePath = FilePath.createTestFilePath();
+    var filePath = FileData.createTestFileData();
     var content = ContentFactory.createEmptyContent(filePath);
     Assertions.assertEquals(filePath, content);
   }
@@ -26,7 +26,7 @@ public class TestContent {
   void testCreationTestContent() {
     var content = ContentFactory.createTestContent();
     Assertions.assertNotNull(content);
-    Assertions.assertNotNull(content.getFilePath());
+    Assertions.assertNotNull(content.getFileData());
     Assertions.assertNotNull(content.getCreationTime());
   }
 }
