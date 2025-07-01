cbd0db3087dc0bdd2a6b132ceed67c2906a91da9
Nikita <Nik-Mir-Vas@yandex.ru>
2025-07-02 00:06:02 +0300

    Finished service

--- src/main/java/com/filecontr/service/virtual_files/VirtualFileService.java
+++ src/main/java/com/filecontr/service/virtual_files/VirtualFileService.java
@@ -3,13 +3,16 @@ package com.filecontr.service.virtual_files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.filecontr.repository.virtual_file.postgres.VirtualFilePostgres;
 import com.filecontr.repository.virtual_file.redis.VirtualFileRedis;
+import com.filecontr.service.server_data.IIdStrategy;
 import com.filecontr.service.server_data.IServerData;
 import com.filecontr.utils.adapters.logger.ILogger;
 import com.filecontr.utils.functional_classes.id.IIdentificator;
@@ -63,8 +66,8 @@ public class VirtualFileService {
 
   @Autowired
   public VirtualFileService(
-    VirtualFilePostgres postgres,
-    VirtualFileRedis redis,
+    @Value("LongtimeDatabase") VirtualFilePostgres postgres,
+    @Value("ColdCache") VirtualFileRedis redis,
     IServerData idServer,
     Function<Class<?>, ILogger> loggerProducer
   ) {
@@ -77,7 +80,7 @@ public class VirtualFileService {
 
   public List<Optional<String>> getVirtualFile(Long... idLong) {
     var ids = Arrays.stream(idLong)
-      .map(a -> IdFactory.createIdFromLong(a))
+      .map(IdFactory::createIdFromLong)
       .toArray(IIdentificator[]::new);
     var virtualFileMap = fileFactory.createVirtualFileById(ids);
     var ret = virtualFileMap.entrySet().stream()
@@ -88,5 +91,31 @@ public class VirtualFileService {
     return Arrays.stream(ret)
       .map(a -> fileFactory.processVirtualFileToJson(a))
       .toList();
+  }
+
+  public List<Optional<String>> createVirtualFile(IIdStrategy... requests) {
+    var vf = Arrays.stream(requests)
+      .map(fileFactory::createNewFileDefault)
+      .toArray(IVirtualFile[]::new);
+    CompletableFuture.runAsync(() -> postgres.addVirtualFile(vf));
+    CompletableFuture.runAsync(() -> redis.addVirtualFile(vf));
+    var ret = Arrays.stream(vf).map(a -> fileFactory.processVirtualFileToJson(a)).toList();
+    return ret;
+  }
+
+  public Boolean setType(Long idLong, String newType) {
+    var id = IdFactory.createIdFromLong(idLong);
+    CompletableFuture.runAsync(() -> postgres.setType(id, newType));
+    CompletableFuture.runAsync(() -> redis.setType(id, newType));
+    return true;
+  }
+
+  public Boolean deleteVirtualFile(Long... idLong) {
+    var ids = Arrays.stream(idLong)
+      .map(IdFactory::createIdFromLong)
+      .toArray(IIdentificator[]::new);
+    CompletableFuture.runAsync(() -> postgres.deleteVirtualFile(ids));
+    CompletableFuture.runAsync(() -> redis.deleteVirtualFile(ids));
+    return true;
   }
 }
